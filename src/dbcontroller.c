857f3c34f390ea6152bd3b343fc7fad4ab270f47
Calata <jorge.cmaeso@alumnos.upm.es>
2018-05-15 12:21:16 +0200

    Comentadno V0.1

--- src/dbcontroller.c
+++ src/dbcontroller.c
@@ -21,7 +21,7 @@
 #define SQL_QUERY_GET_NUMBER_OF_ELEMENTS "SELECT count(*) FROM %s WHERE USERID = %d;"
 /*Prototipo del nombre de la tabla a consultar*/
 #define SQL_TABLE_NAME "TARJETAS"
-/*Tama駉 maximo de query que admite la librer韆 de conexion*/
+/*Tama锟給 maximo de query que admite la librer锟絘 de conexion*/
 #define SQL_QUERY_MAX_SIZE 500
 
 /*
@@ -54,6 +54,12 @@ sqlite3* db_load(char* file_dir){
 	return db;
 }
 
+/**
+ * @brief Ejecuta una query para ver si existe la tabla con el valor especificado
+ * 
+ * @param db Sqlite3 puntero de la estructura cargada
+ * @return int controll code
+ */
 int db_check(sqlite3* db){
 	int rc;
 	char* sql = (char*)malloc(SQL_QUERY_MAX_SIZE*sizeof(char));
@@ -72,9 +78,18 @@ int db_check(sqlite3* db){
 	free(sql);
 	return SQL_OPERATION_OK;
 }
+
+/**
+ * @brief Comprueba si es una base de datos nueva y en caso de serlo crea las tablas necesarias
+ * La funcion comprueba si existen la tabla especificada intentando hacer
+ * la query de creaci贸n, si dicha query se ha ejecutado correctamente, quiere decir
+ * que la base de datos es nueva, por lo que se crean las tablas necesarias para
+ * adecuarla al modelo de datos. Si la ejecuci贸n da error, quiere decir que la base de datos
+ * ya exist铆a y no tienen que sobreescribirse las tablas
+ * @param db Sqlite3 puntero de la estructura cargada
+ */
 /*
- * Comprueba si es una base de datos nueva
- * y en caso de serlo crea las tablas necesarias
+ * 
  * */
 void db_create_tables(sqlite3* db){
 	int rc;
@@ -103,9 +118,14 @@ void db_create_tables(sqlite3* db){
 	free(sql);
 }
 
-/*
- * Ejecuta una query de
- * */
+/**
+ * @brief Ejecuta la query para insertar en la base de datos 
+ * 
+ * @param db Sqlite3 puntero de la estructura cargada
+ * @param user_id Id que se quiere almacenar
+ * @param song_name Nombre del fichero asociado al ID
+ * @return int Codigo de control
+ */
 int db_insert(sqlite3* db,int user_id,char* song_name){
 	char *zErrMsg = 0;
 	int rc;
@@ -125,6 +145,15 @@ int db_insert(sqlite3* db,int user_id,char* song_name){
 	free(sql);
 	return SQL_OPERATION_OK;
 }
+
+/**
+ * @brief Ejecuta la query de busqueda del nombre de la canci贸n dado un ID
+ * Hace una query de busqueda del fichero que esta asociado a un ID
+ * 
+ * @param db Sqlite3 puntero de la estructura cargada 
+ * @param user_id Id de la tarjeta
+ * @return char* nombre de la canci贸n, NULL 
+ */
 
 char* db_get_song_name(sqlite3* db,int user_id){
 	char *zErrMsg = 0;
